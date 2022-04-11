e5d8ad81305b2d1fb4a658750fe20388ca6b7301
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-11 13:17:50 -0300

    test: ensure AddRecordController calls AddRecord usecase with correct value

--- src/presentation/controllers/add-record.spec.ts
+++ src/presentation/controllers/add-record.spec.ts
@@ -1,15 +1,51 @@
-import { AddRecordController } from './add-record'
+import { AddRecordController, AddRecord, HttpRequest } from './add-record-protocols'
+
+interface SutType {
+  sut: AddRecordController
+  addRecordStub: AddRecord
+}
+
+const makeAddRecord = (): AddRecord => {
+  class AddRecordStub implements AddRecord {
+    async add (data: any): Promise<void> {
+      return null
+    }
+  }
+  return new AddRecordStub()
+}
+
+const makeSut = (): SutType => {
+  const addRecordStub = makeAddRecord()
+  const sut = new AddRecordController(addRecordStub)
+  return {
+    sut,
+    addRecordStub
+  }
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    data: 22.9
+  }
+})
 
 describe('AddRecord controller', () => {
-  test('Should return 400 if no data is provided', () => {
-    const sut = new AddRecordController()
+  test('Should return 400 if no data is provided', async () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         data: 22.9
       }
     }
-    const response = sut.handle(httpRequest)
+    const response = await sut.handle(httpRequest)
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new Error('Missing param: data'))
+  })
+
+  test('Should call AddRecord with correct values', async () => {
+    const { sut, addRecordStub } = makeSut()
+    const addSpy = jest.spyOn(addRecordStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith(22.9)
   })
 })
