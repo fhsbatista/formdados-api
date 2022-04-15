41ff974821402475ba69288c34b2c0b37fcfe4cd
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 11:00:52 -0300

    feat: ensure AddRecordController returns 200 if AddRecord succeeds

--- src/presentation/controllers/add-record.ts
+++ src/presentation/controllers/add-record.ts
@@ -5,10 +5,16 @@ export class AddRecordController {
 
   async handle (httpRequest: any): Promise<any> {
     try {
+      const { data } = httpRequest.body
+      if (!data) {
+        return {
+          statusCode: 400,
+          body: new Error('Missing param: data')
+        }
+      }
       await this.addRecord.add(httpRequest.body.data)
       return {
-        statusCode: 400,
-        body: new Error('Missing param: data')
+        statusCode: 200
       }
     } catch (error) {
       return {
