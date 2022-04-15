5aff7f43b5c5afcdefc62677b5b943bb6715b0a1
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 11:23:52 -0300

    test: ensure DBAddRecord calls AddRecordRepository with correct usecases

---
+++ src/data/usecases/add-record/db-add-record.spec.ts
@@ -0,0 +1,34 @@
+import { AddRecordRepository } from '../../protocols/db/record/add-record-repository'
+import { DBAddRecord } from './db-add-record'
+
+const makeAddRecordRepository = (): AddRecordRepository => {
+  class AddRecordRepositoryStub implements AddRecordRepository {
+    async add (data: any): Promise<void> {
+      return new Promise(resolve => resolve(null))
+    }
+  }
+  return new AddRecordRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DBAddRecord
+  addRecordRepositoryStub: AddRecordRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addRecordRepositoryStub = makeAddRecordRepository()
+  const sut = new DBAddRecord(addRecordRepositoryStub)
+  return {
+    sut,
+    addRecordRepositoryStub
+  }
+}
+
+describe('DBAddRecord usecase ', () => {
+  test('Should call AddRecordRepository with correct values', async () => {
+    const { sut, addRecordRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addRecordRepositoryStub, 'add')
+    await sut.add(22.9)
+    expect(addSpy).toHaveBeenCalledWith(22.9)
+  })
+})
