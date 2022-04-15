c5a80daf263edbe2eb0ecefc0f5d1771b6d80d73
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 11:56:43 -0300

    test: ensure RecordMongoRepository's add method returns persisted data on success

--- src/infra/mongodb/record/record-mongo-repository.spec.ts
+++ src/infra/mongodb/record/record-mongo-repository.spec.ts
@@ -5,9 +5,9 @@ const makeSut = (): RecordMongoRepository => {
 }
 
 describe('Record Mongo Repository ', () => {
-  test('Should return null on add success', async () => {
+  test('Should return the persisted data on add success', async () => {
     const sut = makeSut()
     const result = await sut.add(32)
-    expect(result).toBe(null)
+    expect(result).toBe(32)
   })
 })
