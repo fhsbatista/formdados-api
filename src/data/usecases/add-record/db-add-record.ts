8641155824a99941b877af0faf1124bc6bd76557
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 11:23:32 -0300

    feat: ensure DBAddRecord calls AddRecordRepository with correct usecases

---
+++ src/data/usecases/add-record/db-add-record.ts
@@ -0,0 +1,10 @@
+import { AddRecord } from '../../../domain/usecases/add-record'
+import { AddRecordRepository } from '../../protocols/db/record/add-record-repository'
+
+export class DBAddRecord implements AddRecord {
+  constructor (private readonly repository: AddRecordRepository) {}
+
+  async add (data: any): Promise<void> {
+    await this.repository.add(data)
+  }
+}
