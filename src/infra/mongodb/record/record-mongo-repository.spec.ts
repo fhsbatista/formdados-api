b0187edfb3320e8d7f9d06e39c549475798af7de
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 12:31:10 -0300

    test: ensure RecordMongoRepository's add method persist the correct data

--- src/infra/mongodb/record/record-mongo-repository.spec.ts
+++ src/infra/mongodb/record/record-mongo-repository.spec.ts
@@ -1,13 +1,37 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
 import { RecordMongoRepository } from './record-mongo-repository'
 
 const makeSut = (): RecordMongoRepository => {
   return new RecordMongoRepository()
 }
 
+let recordCollection: Collection
+
 describe('Record Mongo Repository ', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    recordCollection = await MongoHelper.getCollection('records')
+    await recordCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('Should return the persisted data on add success', async () => {
     const sut = makeSut()
     const result = await sut.add(32)
     expect(result).toBe(32)
+  })
+
+  test('Should persist the correct data on add', async () => {
+    const sut = makeSut()
+    await sut.add(32)
+    const persisted = MongoHelper.map(await recordCollection.findOne({}))
+    expect(persisted.data).toBe(32)
   })
 })
