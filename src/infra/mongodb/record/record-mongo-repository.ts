2ab766a3f404f4c108be4d999d6152e73c03cb13
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 12:30:48 -0300

    feat: ensure RecordMongoRepository's add method persist the correct data

--- src/infra/mongodb/record/record-mongo-repository.ts
+++ src/infra/mongodb/record/record-mongo-repository.ts
@@ -1,7 +1,13 @@
 import { AddRecordRepository } from '../../../data/protocols/db/record/add-record-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class RecordMongoRepository implements AddRecordRepository {
   async add (data: any): Promise<void> {
-    return data
+    const collection = await MongoHelper.getCollection('records')
+    const result = await collection.insertOne({ data: data })
+    const { insertedId: id } = result
+    const persisted = await collection
+      .findOne({ _id: id })
+    return MongoHelper.map(persisted).data
   }
 }
