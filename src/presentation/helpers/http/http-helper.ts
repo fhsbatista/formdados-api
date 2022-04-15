60ec60bb91c61fed18c932d3a7fcbe8189ff37cb
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 18:47:40 -0300

    chore: add some helpers for http responses

---
+++ src/presentation/helpers/http/http-helper.ts
@@ -0,0 +1,27 @@
+import { ServerError, UnauthorizedError } from '../../errors'
+import { HttpResponse } from '../../protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
