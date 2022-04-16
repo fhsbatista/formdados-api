5f1436849be707879c687d05730e6c7e9d449ffc
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-16 11:23:29 -0300

    feat: ensure AddBfRecordController returns 500 if AddBfRecord usecase throws

--- src/presentation/controllers/add-bf-record/add-bf-record.ts
+++ src/presentation/controllers/add-bf-record/add-bf-record.ts
@@ -1,17 +1,21 @@
-import { badRequest } from '../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../helpers/http/http-helper'
 import { AddBfRecord } from './add-bf-record-protocols'
 
 export class AddBfRecordController {
   constructor (private readonly addBfRecord: AddBfRecord) { }
 
   async handle (httpRequest: any): Promise<any> {
-    const { date, percent } = httpRequest.body
-    if (!date) {
-      return badRequest(new Error('Missing param: date'))
+    try {
+      const { date, percent } = httpRequest.body
+      if (!date) {
+        return badRequest(new Error('Missing param: date'))
+      }
+      if (!percent) {
+        return badRequest(new Error('Missing param: bfPercent'))
+      }
+      await this.addBfRecord.add({ date, percent })
+    } catch (error) {
+      return serverError(error)
     }
-    if (!percent) {
-      return badRequest(new Error('Missing param: bfPercent'))
-    }
-    await this.addBfRecord.add({ date, percent })
   }
 }
