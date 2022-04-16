fa502e0351e5696af9ae726261e2bfd464eb0940
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-15 21:12:38 -0300

    feat: ensure LogControllerDecorator returns serverError if LogErrorRepository throws

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
+import { serverError } from '../../presentation/helpers/http/http-helper'
 
 export class LogControllerDecorator implements Controller {
   constructor (
@@ -10,7 +11,11 @@ export class LogControllerDecorator implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
-      await this.logErrorRepository.logError(httpResponse.body.stack)
+      try {
+        await this.logErrorRepository.logError(httpResponse.body.stack)
+      } catch (error) {
+        return serverError(error)
+      }
     }
     return httpResponse
   }
