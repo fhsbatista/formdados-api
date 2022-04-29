c716b4d6b0b744770c10a69556ec5395218bd1e1
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-29 08:08:43 -0300

    feat: ensure CreateFormController returns 200 when CreateForm succeeds

--- src/presentation/controllers/create-form/create-form.ts
+++ src/presentation/controllers/create-form/create-form.ts
@@ -1,11 +1,15 @@
-import { badRequest } from '../../helpers/http/http-helper'
+import { badRequest, ok } from '../../helpers/http/http-helper'
 import { CreateForm } from './create-form-protocols'
 
 export class CreateFormController {
   constructor (private readonly createForm: CreateForm) { }
 
   async handle (httpRequest: any): Promise<any> {
+    const { fields } = httpRequest.body
+    if (!fields || fields.length === 0) {
+      return badRequest(new Error('Missing param: fields'))
+    }
     await this.createForm.create({ fields: httpRequest.body.fields })
-    return badRequest(new Error('Missing param: fields'))
+    return ok({ message: 'Form created successfully' })
   }
 }
