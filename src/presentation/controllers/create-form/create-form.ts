ceca399c6353ac214296b7e5d36a2b3b5c29d649
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-29 08:20:46 -0300

    feat: ensure CreateFormController returns 500 if usecase throws

--- src/presentation/controllers/create-form/create-form.ts
+++ src/presentation/controllers/create-form/create-form.ts
@@ -1,15 +1,19 @@
-import { badRequest, ok } from '../../helpers/http/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 import { CreateForm } from './create-form-protocols'
 
 export class CreateFormController {
   constructor (private readonly createForm: CreateForm) { }
 
   async handle (httpRequest: any): Promise<any> {
-    const { fields } = httpRequest.body
-    if (!fields || fields.length === 0) {
-      return badRequest(new Error('Missing param: fields'))
+    try {
+      const { fields } = httpRequest.body
+      if (!fields || fields.length === 0) {
+        return badRequest(new Error('Missing param: fields'))
+      }
+      await this.createForm.create({ fields: httpRequest.body.fields })
+      return ok({ message: 'Form created successfully' })
+    } catch (error) {
+      return serverError(error)
     }
-    await this.createForm.create({ fields: httpRequest.body.fields })
-    return ok({ message: 'Form created successfully' })
   }
 }
