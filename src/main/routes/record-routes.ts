57f87dc07a1c93a63600e86d7d74c5eb92b6aec7
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-02 08:56:59 -0300

    feat: create route for CreateFormController

--- src/main/routes/record-routes.ts
+++ src/main/routes/record-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapters'
 import { makeAddBfRecordController } from '../factories/controllers/add-bf-record-controller-factory'
 import { makeAddRecordController } from '../factories/controllers/add-record-controller-factory'
+import { makeCreateFormController } from '../factories/controllers/create-form-controller-factory'
 
 export default (router: Router): void => {
   router.post('/add_record', adaptRoute(makeAddRecordController()))
   router.post('/add_bf_record', adaptRoute(makeAddBfRecordController()))
+  router.post('/create_form', adaptRoute(makeCreateFormController()))
 }
