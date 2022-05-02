55ec6a73d5fec5b94a2288f08a190dd72140eae3
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-02 08:55:30 -0300

    fix: change mongo collection name for forms

--- src/infra/mongodb/form/form-mongo-repository.ts
+++ src/infra/mongodb/form/form-mongo-repository.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class FormMongoRepository implements CreateFormRepository {
   async create (dto: CreateFormDTO): Promise<FormEntity> {
-    const formCollection = await MongoHelper.getCollection('accounts')
+    const formCollection = await MongoHelper.getCollection('forms')
     const result = await formCollection.insertOne(dto)
     const { insertedId: id } = result
     const form = await formCollection
