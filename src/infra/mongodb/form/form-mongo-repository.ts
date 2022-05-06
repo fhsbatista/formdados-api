3290671fe833f2a7f227b91e3f6494445652092b
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-06 08:43:36 -0300

    feat: ensure FormMongoRepository returns list of forms on get

--- src/infra/mongodb/form/form-mongo-repository.ts
+++ src/infra/mongodb/form/form-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { CreateFormRepository } from '../../../data/protocols/db/form/create-form-repository'
+import { GetFormsRepository } from '../../../data/protocols/db/form/get-forms-repository'
 import { FormEntity } from '../../../domain/entities/form-entity'
 import { CreateFormDTO } from '../../../domain/usecases/create-form'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class FormMongoRepository implements CreateFormRepository {
+export class FormMongoRepository implements CreateFormRepository, GetFormsRepository {
   async create (dto: CreateFormDTO): Promise<FormEntity> {
     const formCollection = await MongoHelper.getCollection('forms')
     const result = await formCollection.insertOne(dto)
@@ -11,5 +12,12 @@ export class FormMongoRepository implements CreateFormRepository {
     const form = await formCollection
       .findOne({ _id: id })
     return MongoHelper.map(form)
+  }
+
+  async get (): Promise<FormEntity[]> {
+    const formCollection = await MongoHelper.getCollection('forms')
+    const forms = await formCollection.find<FormEntity>({}).toArray()
+    console.log(forms[0].fields)
+    return forms
   }
 }
