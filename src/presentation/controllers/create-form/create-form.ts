98842b26ac4a12d6322c0825d19305af7172919f
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-11 08:39:01 -0300

    feat: ensure CreateFormController returns 400 is a field which does not contain a name is provided

--- src/presentation/controllers/create-form/create-form.ts
+++ src/presentation/controllers/create-form/create-form.ts
@@ -1,19 +1,32 @@
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
-import { CreateForm } from './create-form-protocols'
+import { CreateForm, FieldDTO, HttpRequest } from './create-form-protocols'
 
 export class CreateFormController {
   constructor (private readonly createForm: CreateForm) { }
 
-  async handle (httpRequest: any): Promise<any> {
+  async handle (httpRequest: HttpRequest): Promise<any> {
     try {
       const { fields } = httpRequest.body
       if (!fields || fields.length === 0) {
         return badRequest(new Error('Missing param: fields'))
+      }
+      if (!this.isFieldsValid(fields)) {
+        return badRequest(new Error('Invalid fields has been provided'))
       }
       await this.createForm.create({ fields: httpRequest.body.fields })
       return ok({ message: 'Form created successfully' })
     } catch (error) {
       return serverError(error)
     }
+  }
+
+  isFieldsValid (fields: FieldDTO[]): boolean {
+    let isFieldsValid = true
+    fields.forEach((field) => {
+      if (!field.name) {
+        isFieldsValid = false
+      }
+    })
+    return isFieldsValid
   }
 }
