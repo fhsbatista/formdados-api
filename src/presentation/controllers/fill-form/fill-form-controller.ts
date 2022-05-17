08176f90f1361a4b49437afc8920c03e625dfc40
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-17 08:43:08 -0300

    feat: ensure FillFormController returns 500 if fill form usecase throws

--- src/presentation/controllers/fill-form/fill-form-controller.ts
+++ src/presentation/controllers/fill-form/fill-form-controller.ts
@@ -1,26 +1,30 @@
-import { badRequest } from '../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { FilledFieldDTO, FillForm } from './fill-form-controller-protocols'
 
 export class FillFormController implements Controller {
-  constructor (private readonly fillForm: FillForm) {}
+  constructor (private readonly fillForm: FillForm) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { formId, filledFields } = httpRequest.body
-    if (!formId) {
-      return badRequest(new Error('Missing param: formId'))
+    try {
+      const { formId, filledFields } = httpRequest.body
+      if (!formId) {
+        return badRequest(new Error('Missing param: formId'))
+      }
+      if (!filledFields) {
+        return badRequest(new Error('Missing param: filledFields'))
+      }
+      if (filledFields.length === 0) {
+        return badRequest(new Error('Invalid param: fields list is empty'))
+      }
+      if (!this.isFilledFieldsValid(filledFields)) {
+        return badRequest(new Error('Invalid param: an invalid filled field has been provided'))
+      }
+      await this.fillForm.fill(httpRequest.body)
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    if (!filledFields) {
-      return badRequest(new Error('Missing param: filledFields'))
-    }
-    if (filledFields.length === 0) {
-      return badRequest(new Error('Invalid param: fields list is empty'))
-    }
-    if (!this.isFilledFieldsValid(filledFields)) {
-      return badRequest(new Error('Invalid param: an invalid filled field has been provided'))
-    }
-    await this.fillForm.fill(httpRequest.body)
-    return null
   }
 
   isFilledFieldsValid (fields: FilledFieldDTO[]): boolean {
