eca83d4d0d975edc1abfd535e81f256f11b2b9ce
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-17 08:40:14 -0300

    feat: ensure FillFormController returns 400 if an invalid filled field is provided

--- src/presentation/controllers/fill-form/fill-form-controller.ts
+++ src/presentation/controllers/fill-form/fill-form-controller.ts
@@ -1,6 +1,6 @@
 import { badRequest } from '../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { FillForm } from './fill-form-controller-protocols'
+import { FilledFieldDTO, FillForm } from './fill-form-controller-protocols'
 
 export class FillFormController implements Controller {
   constructor (private readonly fillForm: FillForm) {}
@@ -16,7 +16,20 @@ export class FillFormController implements Controller {
     if (filledFields.length === 0) {
       return badRequest(new Error('Invalid param: fields list is empty'))
     }
+    if (!this.isFilledFieldsValid(filledFields)) {
+      return badRequest(new Error('Invalid param: an invalid filled field has been provided'))
+    }
     await this.fillForm.fill(httpRequest.body)
     return null
+  }
+
+  isFilledFieldsValid (fields: FilledFieldDTO[]): boolean {
+    let isFilledFieldsValid = true
+    fields.forEach((field) => {
+      if (!field.fieldName || !field.value) {
+        isFilledFieldsValid = false
+      }
+    })
+    return isFilledFieldsValid
   }
 }
