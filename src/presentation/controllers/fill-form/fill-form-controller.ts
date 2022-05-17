5e7aacfb61a9ba68754e21f55ae8ad5ea6ae8a8a
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-17 08:28:31 -0300

    feat: ensure FillFormController returns 400 if formId is not provided

--- src/presentation/controllers/fill-form/fill-form-controller.ts
+++ src/presentation/controllers/fill-form/fill-form-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { FillForm } from './fill-form-controller-protocols'
 
@@ -5,6 +6,10 @@ export class FillFormController implements Controller {
   constructor (private readonly fillForm: FillForm) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { formId } = httpRequest.body
+    if (!formId) {
+      return badRequest(new Error('Missing param: formId'))
+    }
     await this.fillForm.fill(httpRequest.body)
     return null
   }
